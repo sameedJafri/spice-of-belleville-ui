a566137efc43fb37ce5b932aee9e81e4ac49e4d0
Sameed Jafri <sameedjafri@hotmail.com>
2026-08-17 00:11:06 -0400

    adding css styles to navbar and adding icon to tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Spice Of Belleville",
@@ -20,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>
+    <html lang="en">
+      <body className="app">
         <Navbar />
         {children}
       </body>
