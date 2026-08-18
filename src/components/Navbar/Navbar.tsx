7c769fd946f716de922f51c69502d280379fffc8
Sameed Jafri <sameedjafri@hotmail.com>
2026-08-17 23:55:36 -0400

    add navbar hover effects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,46 +1,61 @@
+"use client";
+
 import React from "react";
 import styles from "./Navbar.module.css";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
 import cart from "@/assets/cart.webp";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { href: "/about", label: "About" },
+  { href: "/menu", label: "Menu" },
+  { href: "/contact", label: "Contact" },
+];
 
 const Navbar: React.FC = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className={styles.navbar}>
-      <Image
-        className={styles.logo}
-        src={logo}
-        alt="logo"
-        width={100}
-        height={100}
-        priority
-      />
-      <ul className={styles.navLinks}>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/menu">Menu</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-      <div className={styles.navRight}>
-        {/* add search image with next/image and link to search page */}
-        <div className={styles.cart}>
-          <Image
-            src={cart}
-            alt="cart"
-            width={20}
-            height={20}
-          />
-          <div className={styles.dot}></div>
+    <header>
+      <nav className={styles.navbar}>
+        <Image
+          className={styles.logo}
+          src={logo}
+          alt="logo"
+          width={100}
+          height={100}
+          priority
+        />
+        <ul className={styles.navLinks}>
+          {links.map(({ href, label }) => (
+            <li key={href}>
+              <Link
+                href={href}
+                className={pathname === href ? styles.active : undefined}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.navRight}>
+          {/* add search image with next/image and link to search page */}
+          <div className={styles.cart}>
+            <Image
+              src={cart}
+              alt="cart"
+              width={20}
+              height={20}
+            />
+            <div className={styles.dot}></div>
+          </div>
+          <button>Sign In</button>
         </div>
-        <button>Sign In</button>
-      </div>
-    </nav>
+      </nav>
+    </header>
+
   );
 };
 
