39e56544789451251b2a243c3b266087d88b22a3
Sameed Jafri <sameedjafri@hotmail.com>
2026-08-17 23:55:56 -0400

    add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <>
       <main className={styles.main}>
+        <Header />
       </main>
-    </div>
+    </>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+
+const Header: React.FC = () => {
+
+    return (
+        <div className={styles.header}>
+            <div className={styles.headerContent}>
+                <h2>What are you craving?</h2>
+                <div className={styles.headerSearchbar}>
+                    <input type="text" placeholder="Biryani, Karahi, Kabobs, BBQ, Samosas..." />
+                    <button>Search</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
