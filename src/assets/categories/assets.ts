b9c6306a3674de00fcf75928eda2aa4e1781b668
Sameed Jafri <sameedjafri@hotmail.com>
2026-08-19 18:26:42 -0400

    add menu categories

---
+++ src/assets/categories/assets.ts
@@ -0,0 +1,21 @@
+import appitizers from "./appitizers.png";
+import platters from "./platters.png";
+import kabobs from "./kabobs.png";
+import rice from "./rice.png";
+import bbq from "./bbq.png";
+import grains from "./grains.webp";
+import curry from "./curry.png";
+import soups from "./soups.png";
+import deserts from "./deserts.png";
+
+export const categoryImages = {
+    appitizers,
+    platters,
+    kabobs,
+    rice,
+    bbq,
+    grains,
+    curry,
+    soups,
+    deserts,
+};
--- src/components/Browsemenu/Browsemenu.tsx
+++ src/components/Browsemenu/Browsemenu.tsx
@@ -1,13 +1,41 @@
 import React from "react";
 import styles from "./Browsemenu.module.css";
+import Image from "next/image";
+import { categoryImages } from "@/assets/categories/assets";
 
 const Browsemenu: React.FC = () => {
 
+    const categories = [
+        { key: "appitizers", image: categoryImages.appitizers },
+        { key: "platters", image: categoryImages.platters },
+        { key: "Kabaobs", image: categoryImages.kabobs },
+        { key: "Rice", image: categoryImages.rice },
+        { key: "BBQ", image: categoryImages.bbq },
+        { key: "Grains", image: categoryImages.grains },
+        { key: "Curry", image: categoryImages.curry },
+        { key: "Soups", image: categoryImages.soups },
+        { key: "Desserts", image: categoryImages.deserts },
+
+    ];
+
+    const categoryItems = categories.map((category) => (
+        <div key={category.key} className={styles.categoryItem}>
+            <Image src={category.image} alt={category.key} width={200} height={200} />
+            <h3>{category.key}</h3>
+        </div>
+    ));
+
+
     return (
         <div className={styles.browseMenu}>
-            <h2>Browse Menu Categories</h2>
-            <div className={styles.browseMenuBorder}></div>
-        </div>
+            <div className={styles.browseMenuHeader}>
+                <h2>Browse Menu Categories</h2>
+                <div className={styles.browseMenuBorder}></div>
+            </div>
+            <div className={styles.browseMenuItemList}>
+                {categoryItems}
+            </div>
+        </div >
     );
 };
 
